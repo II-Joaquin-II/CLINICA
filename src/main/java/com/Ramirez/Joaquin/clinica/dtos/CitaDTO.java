0d9f0d43c58365d16bceca15c12072561b18a770
II-Joaquin-II <U22221498@UTP.EDU.PE>
2026-08-07 20:07:12 -0500

    agregue anotaciones de validación a las propiedades de la citaDTO

--- src/main/java/com/Ramirez/Joaquin/clinica/dtos/CitaDTO.java
+++ src/main/java/com/Ramirez/Joaquin/clinica/dtos/CitaDTO.java
@@ -2,13 +2,23 @@ package com.Ramirez.Joaquin.clinica.dtos;
 
 import java.time.LocalDateTime;
 
+import jakarta.validation.constraints.Future;
+import jakarta.validation.constraints.NotNull;
+
 public class CitaDTO {
 
+    @NotNull(message = "La fecha y hora son obligatorias")
+    @Future(message = "La cita debe programarse para una fecha futura")
     private LocalDateTime fechaHora;
+
+    @NotNull(message = "El motivo de la cita no puede estar vacío")
     private String motivo;
     
     //IDs de los objetos relacionados
+    @NotNull(message = "El ID del paciente es obligatorio")
     private Long pacienteId;
+    
+    @NotNull(message = "El ID del médico es obligatorio")
     private Long medicoId;
 
     public CitaDTO() {
